3c70793f5dc5e2632f3aa8fbe92721a6fcea5065
rlsalcido24 <rsalcido@alumni.stanford.edu>
2024-02-02 10:46:48 -0800

    Update tablecreate.sql

--- tablecreate.sql
+++ tablecreate.sql
@@ -1,12 +1,6 @@
-CREATE WIDGET TEXT cat DEFAULT "financeclone";
-
-CREATE WIDGET TEXT sch DEFAULT "dev";
 
 
 create table ${cat}.${sch}.filetest
   as select * from samples.tpch.customers
   limit 100;
 
-create table ${cat}.${sch}.filetestuno
-  as select * from samples.tpch.customers
-  limit 100;
