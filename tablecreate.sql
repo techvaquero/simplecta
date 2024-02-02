0be8d773df7345555bcc29afc2405ba1fe4e4c96
rlsalcido24 <rsalcido@alumni.stanford.edu>
2024-02-02 10:45:34 -0800

    Update tablecreate.sql

--- tablecreate.sql
+++ tablecreate.sql
@@ -1,3 +1,12 @@
-create table identifier(testcatalog).identifier(testschmea).vartest
+CREATE WIDGET TEXT cat DEFAULT "financeclone";
+
+CREATE WIDGET TEXT sch DEFAULT "dev";
+
+
+create table ${cat}.${sch}.filetest
   as select * from samples.tpch.customers
-  limit 100
+  limit 100;
+
+create table ${cat}.${sch}.filetestuno
+  as select * from samples.tpch.customers
+  limit 100;
