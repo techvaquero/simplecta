29f3da95a3724d28389888b599dad88210363bc2
techvaquero <rsalcido@alumni.stanford.edu>
2025-01-14 14:13:01 -0800

    Update dbut.py

--- dbut.py
+++ dbut.py
@@ -1,4 +1,3 @@
-import vertica_python
 import os
 import re
 import json
@@ -15,43 +14,43 @@ from databricks import sql
 
 
 
-def process_file(verticaqueryfile:str):
+def process_file(dbqueryfile:str):
   # This is the main function that invokes the regex templates to convert the dialect. the transpiler is distributed so each file is transformed independently before being written out.
    
   
-  with open(verticaqueryfile, 'r+') as file:
+  with open(dbqueryfile, 'r+') as file:
     content = file.read()
 
-    with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
-                 http_path       = os.getenv("DATABRICKS_HTTP_PATH"),
-                 access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
+    with sql.connect(server_hostname = "xxx",
+                 http_path       = "yyy",
+                 access_token    = "zzz") as connection:
 
       with connection.cursor() as cursor:
-      cursor.execute(content)
-      rows = cursor.fetchall()
-      querycount = rows.length()
-      dqdict = {'path': verticaqueryfile, 'count': querycount}
-      utlist.append(dqdict)
+        cursor.execute(content)
+        rows = cursor.fetchall()
+        querycount = rows.length()
+        dqdict = {'path': dbqueryfile, 'count': querycount}
+        utlist.append(dqdict)
 
 
-def runner(verticaquerydir:str):
+def runner(dbquerydir:str):
   # This is the runner function that first finds files to parse and then invokes the main transpiler methods to convert the dialect. the transpiler is distributed so each file is transformed independently before being written out.
 
   ## The package version assumes the utility helper lives under /project_folder/packages/lakehouse_utils/helper/. So it looks 3 levels up for a dbt_project file and uses that as the base directory
  
-  files = find_files(verticaquerydir)
+  files = find_files(dbquerydir)
   files_array = []
   with ThreadPoolExecutor() as executor:
     futures_sql = {executor.submit(process_file, p): p for p in files}
   utilsdf = spark.createDataFrame(utlist)
-  utilsdf.write.mode("Overwrite").saveAsTable("test.non_vertica_sr.vertutresults") 
+  utilsdf.write.saveAsTable("prodrs.default.dbxutresults") 
   print('woohoo, weve reached the end of the road (but still i cant let go)') 
 
 
 
-def find_files(verticaquerydir:str):
+def find_files(dbquerydir:str):
     # Convert the input to a Path object
-    path = Path(verticaquerydir)
+    path = Path(dbquerydir)
 
     # Check if the provided path is a directory
     if not path.is_dir():
@@ -69,8 +68,7 @@ def find_files(verticaquerydir:str):
 
 if __name__ == '__main__':
 
-    verticaquerydir = 'pathhere'
+    dbquerydir = '/Workspace/Users/roberto.salcido@databricks.com/simplecta/dbqueries/noparams'
     utlist = []
 
     ## Now do project conversion
-    runner(verticaquerydir = verticaquerydir)
